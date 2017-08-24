30c6c31e59ef8de71fe49106eda85ea269711595
Dmitry Andriyanov <dima117a@yandex-team.ru>
2017-08-24 20:53:33 +0300

    the first implementation

---
+++ example-tests.js
@@ -0,0 +1,179 @@
+'use strict';
+
+const assert = chai.assert;
+
+describe('Array', function() {
+    describe('#indexOf()', function() {
+        it('should return -1 when the value is not present', function() {
+            for (var i = 0; i < 1000000; i++);
+            assert.equal(-1, [1, 2, 3].indexOf(4));
+        });
+        it('should return -1 when the value is not present', function() {
+            for (var i = 0; i < 1000000; i++);
+            assert.equal(-1, [1, 2, 3].indexOf(4));
+        });
+        it('should return -1 when the value is not present', function() {
+            for (var i = 0; i < 1000000; i++);
+            assert.equal(-1, [1, 2, 3].indexOf(4));
+        });
+        it('should return -1 when the value is not present', function() {
+            for (var i = 0; i < 1000000; i++);
+            assert.equal(-1, [1, 2, 3].indexOf(4));
+        });
+        it('should return -1 when the value is not present', function() {
+            for (var i = 0; i < 1000000; i++);
+            assert.equal(-1, [1, 2, 3].indexOf(4));
+        });
+        it('should return -1 when the value is not present', function() {
+            for (var i = 0; i < 1000000; i++);
+            assert.equal(-1, [1, 2, 3].indexOf(4));
+        });
+        it('should return -1 when the value is not present', function() {
+            for (var i = 0; i < 1000000; i++);
+            assert.equal(-1, [1, 2, 3].indexOf(4));
+        });
+        it('should return -1 when the value is not present', function() {
+            for (var i = 0; i < 1000000; i++);
+            assert.equal(-1, [1, 2, 3].indexOf(4));
+        });
+        it('should return -1 when the value is not present', function() {
+            for (var i = 0; i < 1000000; i++);
+            assert.equal(-1, [1, 2, 3].indexOf(4));
+        });
+        it('should return -1 when the value is not present', function() {
+            for (var i = 0; i < 1000000; i++);
+            assert.equal(-1, [1, 2, 3].indexOf(4));
+        });
+        it('should return -1 when the value is not present', function() {
+            for (var i = 0; i < 1000000; i++);
+            assert.equal(-1, [1, 2, 3].indexOf(4));
+        });
+        it('should return -1 when the value is not present', function() {
+            for (var i = 0; i < 1000000; i++);
+            assert.equal(-1, [1, 2, 3].indexOf(4));
+        });
+        it('should return -1 when the value is not present', function() {
+            for (var i = 0; i < 1000000; i++);
+            assert.equal(-1, [1, 2, 3].indexOf(4));
+        });
+        it('should return -1 when the value is not present', function() {
+            for (var i = 0; i < 1000000; i++);
+            assert.equal(-1, [1, 2, 3].indexOf(4));
+        });
+        it('should return -1 when the value is not present', function() {
+            for (var i = 0; i < 1000000; i++);
+            assert.equal(-1, [1, 2, 3].indexOf(4));
+        });
+        it('should return -1 when the value is not present', function() {
+            for (var i = 0; i < 1000000; i++);
+            assert.equal(-1, [1, 2, 3].indexOf(4));
+        });
+        it('should return -1 when the value is not present', function() {
+            for (var i = 0; i < 1000000; i++);
+            assert.equal(-1, [1, 2, 3].indexOf(4));
+        });
+        it('should return -1 when the value is not present', function() {
+            for (var i = 0; i < 1000000; i++);
+            assert.equal(-1, [1, 2, 3].indexOf(4));
+        });
+        it('should return -1 when the value is not present', function() {
+            for (var i = 0; i < 1000000; i++);
+            assert.equal(-1, [1, 2, 3].indexOf(4));
+        });
+        it('should return -1 when the value is not present', function() {
+            for (var i = 0; i < 1000000; i++);
+            assert.equal(-1, [1, 2, 3].indexOf(4));
+        });
+        it('should return -1 when the value is not present', function() {
+            for (var i = 0; i < 1000000; i++);
+            assert.equal(-1, [1, 2, 3].indexOf(4));
+        });
+        it('should return -1 when the value is not present', function() {
+            for (var i = 0; i < 1000000; i++);
+            assert.equal(-1, [1, 2, 3].indexOf(4));
+        });
+        it('should return -1 when the value is not present', function() {
+            for (var i = 0; i < 1000000; i++);
+            assert.equal(-1, [1, 2, 3].indexOf(4));
+        });
+        it('should return -1 when the value is not present', function() {
+            for (var i = 0; i < 1000000; i++);
+            assert.equal(-1, [1, 2, 3].indexOf(4));
+        });
+        it('should return -1 when the value is not present', function() {
+            for (var i = 0; i < 1000000; i++);
+            assert.equal(-1, [1, 2, 3].indexOf(4));
+        });
+        it('should return -1 when the value is not present', function() {
+            for (var i = 0; i < 1000000; i++);
+            assert.equal(-1, [1, 2, 3].indexOf(4));
+        });
+        it('should return -1 when the value is not present', function() {
+            for (var i = 0; i < 1000000; i++);
+            assert.equal(-1, [1, 2, 3].indexOf(4));
+        });
+        it('should return -1 when the value is not present', function() {
+            for (var i = 0; i < 1000000; i++);
+            assert.equal(-1, [1, 2, 3].indexOf(4));
+        });
+        it('should return -1 when the value is not present', function() {
+            for (var i = 0; i < 1000000; i++);
+            assert.equal(-1, [1, 2, 3].indexOf(4));
+        });
+        it('should return -1 when the value is not present', function() {
+            for (var i = 0; i < 1000000; i++);
+            assert.equal(-1, [1, 2, 3].indexOf(4));
+        });
+        it('should return -1 when the value is not present', function() {
+            for (var i = 0; i < 1000000; i++);
+            assert.equal(-1, [1, 2, 3].indexOf(4));
+        });
+        it('should return -1 when the value is not present', function() {
+            for (var i = 0; i < 1000000; i++);
+            assert.equal(-1, [1, 2, 3].indexOf(4));
+        });
+        it('should return -1 when the value is not present', function() {
+            for (var i = 0; i < 1000000; i++);
+            assert.equal(-1, [1, 2, 3].indexOf(4));
+        });
+        it('should return -1 when the value is not present', function() {
+            for (var i = 0; i < 1000000; i++);
+            assert.equal(-1, [1, 2, 3].indexOf(4));
+        });
+        it('should fail111111111', function() {
+            assert.equal(3, [1, 2, 3].indexOf(44));
+        });
+        it('should return -1 when the value is not present', function() {
+            for (var i = 0; i < 1000000; i++);
+            assert.equal(-1, [1, 2, 3].indexOf(4));
+        });
+        it('should return -1 when the value is not present', function() {
+            for (var i = 0; i < 1000000; i++);
+            assert.equal(-1, [1, 2, 3].indexOf(4));
+        });
+        it('should return -1 when the value is not present', function() {
+            for (var i = 0; i < 1000000; i++);
+            assert.equal(-1, [1, 2, 3].indexOf(4));
+        });
+        it('should return -1 when the value is not present', function() {
+            for (var i = 0; i < 1000000; i++);
+            assert.equal(-1, [1, 2, 3].indexOf(4));
+        });
+        it('should return -1 when the value is not present', function() {
+            for (var i = 0; i < 1000000; i++);
+            assert.equal(-1, [1, 2, 3].indexOf(4));
+        });
+        it('should return -1 when the value is not present', function() {
+            for (var i = 0; i < 1000000; i++);
+            assert.equal(-1, [1, 2, 3].indexOf(4));
+        });
+        it('should fail', function() {
+            // console.log('kfjsahfkjashkfjhaskjfhkasjhfkajshfkjashfkjashfkjashkfjhaskjfhaksjfhkajshfkahsjkf');
+            // console.log('kfjsahfkjashkfjhaskjfhkasjhfkajshfkjashfkjashfkjashkfjhaskjfhaksjfhkajshfkahsjkf');
+            // console.log('kfjsahfkjashkfjhaskjfhkasjhfkajshfkjashfkjashfkjashkfjhaskjfhaksjfhkajshfkahsjkf');
+            // console.log('kfjsahfkjashkfjhaskjfhkasjhfkajshfkjashfkjashfkjashkfjhaskjfhaksjfhkajshfkahsjkf');
+            // console.log('kfjsahfkjashkfjhaskjfhkasjhfkajshfkjashfkjashfkjashkfjhaskjfhaksjfhkajshfkahsjkf');
+            assert.equal(3, [1, 2, 3].indexOf(44));
+        });
+    });
+});
---
+++ runner.js
@@ -0,0 +1,74 @@
+'use strict';
+
+const path = require('path');
+const util = require('util');
+const puppeteer = require('puppeteer');
+
+const url = path.resolve('example-page.html');
+
+function initMocha() {
+
+    window.runMochaHeadlessChrome = function() {
+        // var MARK = '#mocha#';
+        //
+        // function MyReporter(runner) {
+        //     Mocha.reporters.Base.call(this, runner);
+        //     runner.on('pass', test => console.log(MARK, 'pass', test));
+        //     runner.on('fail', (test, err) => console.log(MARK, 'fail', test, err));
+        //     runner.on('end', test => console.log(MARK, 'end'));
+        // }
+        //
+        // //mocha.setup({ reporter: MyReporter });
+
+        Mocha.reporters.Base.useColors = true;
+        Mocha.reporters.Base.symbols.dot = '.';
+        Mocha.reporters.Base.symbols.ok = '✓';
+        Mocha.reporters.Base.symbols.err = '✖';
+        Mocha.reporters.Base.symbols.dot = '․';
+        Mocha.reporters.Base.symbols.comma = ',';
+        Mocha.reporters.Base.symbols.bang = '!';
+
+        mocha.setup({ reporter: Mocha.reporters.dot });
+        mocha.run().on('end', () => window.testsCompleted = true);
+    };
+}
+
+
+(async () => {
+    const browser = await puppeteer.launch({ ignoreHTTPSErrors: true, headless: true });
+    const page = await browser.newPage();
+
+    page.on('console', (...args) => {
+        //console.log('>>>>>>>>>>>>>>>>>>begin', JSON.stringify(args));
+
+        let isStdout = args[0] === 'stdout:';
+        isStdout && (args = args.slice(1));
+
+        let msg = util.format(...args);
+        !isStdout && (msg += '\n');
+        process.stdout.write(msg);
+    });
+
+    page.on('dialog', async dialog => {
+        await dialog.dismiss();
+    });
+
+    page.on('pageerror', async err => {
+        console.error(err);
+    });
+
+    // page.on('request', request => {
+    //   if (/\.(png|jpg|jpeg|gif|webp)$/i.test(request.url))
+    //     request.abort();
+    //   else
+    //     request.continue();
+    // });
+
+    await page.evaluateOnNewDocument(initMocha);
+
+    await page.goto(`file://${url}`);
+    await page.waitForFunction(() => window.testsCompleted);
+
+    process.stdout.write('\n');
+    browser.close();
+})();
