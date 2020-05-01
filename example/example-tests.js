70884289298276a8dd32cedbefafade5433780b0
Dmitry Andriyanov <dima117a@yandex-team.ru>
2020-05-01 10:33:01 +0300

    Fix the error when console output arguments is not serializable

--- example/example-tests.js
+++ example/example-tests.js
@@ -38,6 +38,7 @@ describe('Array', function() {
                 it(`(${i}) should return -1 when the value is not present`, function() {
                     delay();
                     assert.equal(-1, [1, 2, 3].indexOf(4));
+                    console.warn(window);
                 });
             }
         }
